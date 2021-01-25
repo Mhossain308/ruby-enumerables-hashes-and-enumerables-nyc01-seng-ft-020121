b7db6ccf287d39239ce33861895580a5e242b5fb
Mohamed Hossain <mz.hossain212@gmail.com>
2021-01-25 07:48:13 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,7 @@
 
 def happy_birthday(birthday_kids)
  birthday_kids.each do |name, age|
-   puts "Happy Birthday #{Amanda}! You are now #{27} years old!"
+   puts "Happy Birthday #{name}! You are now #{age} years old!"
 end
 birthday_kids
 end
